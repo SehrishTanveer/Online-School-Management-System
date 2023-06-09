5599403e8a653e76babf94146f47e369bc9e29fd
Sehrish tanveer <Sehrish tanveer@192.168.56.1>
2023-06-09 17:59:43 +0500

    SCD Final Project

---
+++ src/group/pkg9/bse203123/bse203144/bse203163/GROUP9BSE203123BSE203144BSE203163.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package group.pkg9.bse203123.bse203144.bse203163;
+
+/**
+ *
+ * @author Sehrish tanveer
+ */
+public class GROUP9BSE203123BSE203144BSE203163 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" ONLINE SCHOOL MANAGEMENT SYSTEM");
+    }
+    
+}
